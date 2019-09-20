62401d3258f666b69d86a4d5f05b72ac1471bf21
Szymon Janota <szymon.janota@sap.com>
2019-09-20 20:47:27 +0200

    Fix eslintrc

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     },
     propWrapperFunctions: [
         // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-        forbidExtraProps,
+        "forbidExtraProps",
         {property: "freeze", object: "Object"},
         {property: "myFavoriteWrapper"}
     ],
