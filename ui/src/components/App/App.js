e905c98234d859cd848b1026568bee3fe961a059
Szymon Janota <szymon.janota@sap.com>
2019-09-20 19:10:19 +0200

    AccountsList placeholder

--- ui/src/components/App/App.js
+++ ui/src/components/App/App.js
@@ -3,7 +3,7 @@ import ExpensesList from "../ExpensesList/ExpensesList";
 import {Navigation} from "../Navigation/Navigation";
 import './App.css';
 import {Switch} from "react-router-dom";
-import Route from "react-router-dom/es/Route";
+import {Route} from "react-router-dom";
 
 export default function App() {
   return (
@@ -12,6 +12,7 @@ export default function App() {
       <div className={"App-main"}>
         <Switch>
           <Route path={"/expenses"} component={ExpensesList}/>
+          <Route path={"/accounts"} render={() => <p>Tu będzie lista kont</p>}/>
         </Switch>
       </div>
     </div>
