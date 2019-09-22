3f160b5aa2668e00f501677fba2c57e74561652a
Szymon Janota <szymon.janota@sap.com>
2019-09-22 12:59:21 +0200

    Move schema to backend

--- ui/apollo.config.js
+++ ui/apollo.config.js
@@ -1,12 +1,10 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    client: {
-        service: {
-            localSchemaFile: '../schema.graphql',
-            endpoint: null
-        },
-        includes: [
-            "src/**/*.js"
-        ]
-    }
+  client: {
+    service: {
+      localSchemaFile: '../backend/schema.graphql',
+      endpoint: null,
+    },
+    includes: ['src/**/*.js'],
+  },
 };
