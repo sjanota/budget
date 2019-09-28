8a58fb9af7c65074ddfe9c23d202379a709ea60c
Szymon Janota <szymon.janota@sap.com>
2019-09-28 19:26:34 +0200

    Display both envelopes and categories in one row

--- ui/src/components/EnvelopesPage/EnvelopesPage.js
+++ ui/src/components/EnvelopesPage/EnvelopesPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { EnvelopesList } from '../EnvelopesList/EnvelopesList';
 import { CategoriesList } from '../CategoriesList/CategoriesList';
+import './EnvelopesPage.css';
 
 export function EnvelopesPage() {
   return (
