e57f655bfd62dee414f14bec468195d09a1d9e9f
Szymon Janota <szymon.janota@sap.com>
2019-09-29 18:30:13 +0200

    Refactor modal

--- ui/src/components/ExpensesList/EditModalContent.js
+++ ui/src/components/ExpensesList/EditModalContent.js
@@ -19,6 +19,125 @@ export function EditModalContent({ init, onCancel, onSubmit, autoFocusRef }) {
     return setState(e => ({ ...e, ...value }));
   }
 
+  const inputProps = {
+    state,
+    setValue,
+  };
+
+  return (
+    <>
+      <Modal.Header>Nowy wydatek</Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Row>
+            <TitleInput {...inputProps} autoFocusRef={autoFocusRef} />
+          </Row>
+          <Row>
+            <DateInput {...inputProps} />
+            <AccountInput {...inputProps} />
+          </Row>
+          <hr />
+          <Row>
+            <CategoriesInput state={state} setState={setState} />
+          </Row>
+          <hr />
+          <Row>
+            <SumOutput state={state} />
+          </Row>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <CancelButton onCancel={onCancel} />
+        <SubmitButton state={state} onSubmit={onSubmit} onCancel={onCancel} />
+      </Modal.Footer>
+    </>
+  );
+}
+
+EditModalContent.propTypes = {
+  init: PropTypes.shape({
+    title: PropTypes.string,
+    date: PropTypes.string,
+    totalBalance: MoneyAmountPropType,
+    entries: PropTypes.arrayOf(EditModalContentEntry.propTypes.entry),
+  }),
+  onCancel: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  autoFocusRef: PropTypes.any,
+};
+
+function TitleInput({ state, autoFocusRef, setValue }) {
+  return (
+    <Col>
+      <Form.Label>Tytuł</Form.Label>
+      <Form.Control
+        ref={autoFocusRef}
+        type="text"
+        placeholder="Tytuł"
+        value={state.title}
+        onChange={e => setValue({ title: e.target.value })}
+      />
+    </Col>
+  );
+}
+TitleInput.propTypes = {
+  state: PropTypes.shape({ title: PropTypes.string.isRequired }),
+  setValue: PropTypes.func.isRequired,
+  autoFocusRef: EditModalContent.propTypes.autoFocusRef,
+};
+
+function DateInput({ state, setValue }) {
+  return (
+    <Col>
+      <Form.Label>Data</Form.Label>
+      <Form.Control
+        type="date"
+        placeholder="Data"
+        value={state.date}
+        onChange={e => setValue({ date: e.target.value })}
+      />
+    </Col>
+  );
+}
+DateInput.propTypes = {
+  state: PropTypes.shape({ date: PropTypes.string.isRequired }),
+  setValue: PropTypes.func.isRequired,
+};
+
+function AccountInput() {
+  return (
+    <Col>
+      <Form.Label>Konto</Form.Label>
+      <Form.Control
+        type="text"
+        placeholder="Under construction"
+        readOnly={true}
+      />
+    </Col>
+  );
+}
+AccountInput.propTypes = {};
+
+function CreateCategoryButton({ setState }) {
+  return (
+    <CreateButton
+      onClick={() =>
+        setState(s => ({
+          ...s,
+          entries: [
+            ...s.entries,
+            { title: '', balance: '0.0', categoryID: '' },
+          ],
+        }))
+      }
+    />
+  );
+}
+CreateCategoryButton.propTypes = {
+  setState: PropTypes.func.isRequired,
+};
+
+function CategoriesInput({ state, setState }) {
   function setEntry(idx, update) {
     return setState(s => {
       const entries = replaceOnList(s.entries, idx, {
@@ -38,100 +157,74 @@ export function EditModalContent({ init, onCancel, onSubmit, autoFocusRef }) {
   }
 
   return (
+    <Col>
+      <Form.Label>Kategorie</Form.Label>
+      <CreateCategoryButton setState={setState} />
+      {state.entries.map((entry, idx) => (
+        <EditModalContentEntry
+          key={idx}
+          entry={entry}
+          idx={idx}
+          setEntry={setEntry}
+        />
+      ))}
+    </Col>
+  );
+}
+CategoriesInput.propTypes = {
+  setState: PropTypes.func.isRequired,
+  state: PropTypes.shape({
+    entries: PropTypes.arrayOf(EditModalContentEntry.propTypes.entry),
+  }),
+};
+
+function SumOutput({ state }) {
+  return (
     <>
-      <Modal.Header>Nowy wydatek</Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Label>Tytuł</Form.Label>
-          <Form.Control
-            ref={autoFocusRef}
-            type="text"
-            placeholder="Tytuł"
-            value={state.title}
-            onChange={e => setValue({ title: e.target.value })}
-          />
-          <Row>
-            <Col>
-              <Form.Label>Data</Form.Label>
-              <Form.Control
-                type="date"
-                placeholder="Data"
-                value={state.date}
-                onChange={e => setValue({ date: e.target.value })}
-              />
-            </Col>
-            <Col>
-              <Form.Label>Konto</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Under construction"
-                readOnly={true}
-              />
-            </Col>
-          </Row>
-          <hr />
-          <Form.Label>Kategorie</Form.Label>
-          <CreateButton
-            onClick={() =>
-              setState(s => ({
-                ...s,
-                entries: [
-                  ...s.entries,
-                  { title: '', balance: '0.0', categoryID: '' },
-                ],
-              }))
-            }
-          />
-          {state.entries.map((entry, idx) => (
-            <EditModalContentEntry
-              key={idx}
-              entry={entry}
-              idx={idx}
-              setEntry={setEntry}
-            />
-          ))}
-          <hr />
-          <Row>
-            <Col>
-              <Form.Label>Suma</Form.Label>
-            </Col>
-            <Col>
-              <Form.Control
-                type="number"
-                placeholder="Suma"
-                value={MoneyAmount.format(state.totalBalance)}
-                readOnly={true}
-              />
-            </Col>
-          </Row>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={onCancel}>
-          Anuluj
-        </Button>
-        <Button
-          variant="primary"
-          onClick={() => {
-            onSubmit(state);
-            onCancel();
-          }}
-        >
-          Zapisz
-        </Button>
-      </Modal.Footer>
+      <Col>
+        <Form.Label>Suma</Form.Label>
+      </Col>
+      <Col>
+        <Form.Control
+          type="number"
+          placeholder="Suma"
+          value={MoneyAmount.format(state.totalBalance)}
+          readOnly={true}
+        />
+      </Col>
     </>
   );
 }
+SumOutput.propTypes = {
+  state: PropTypes.shape({ totalBalance: MoneyAmountPropType }),
+};
 
-EditModalContent.propTypes = {
-  init: PropTypes.shape({
-    title: PropTypes.string,
-    date: PropTypes.string,
-    totalBalance: MoneyAmountPropType,
-    entries: PropTypes.arrayOf(EditModalContentEntry.propTypes.entry),
-  }),
-  onCancel: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  autoFocusRef: PropTypes.any,
+function CancelButton({ onCancel }) {
+  return (
+    <Button variant="secondary" onClick={onCancel}>
+      Anuluj
+    </Button>
+  );
+}
+CancelButton.propTypes = {
+  onCancel: EditModalContent.propTypes.onCancel,
+};
+
+function SubmitButton({ state, onSubmit, onCancel }) {
+  return (
+    <Button
+      variant="primary"
+      onClick={() => {
+        onSubmit(state);
+        onCancel();
+      }}
+    >
+      Zapisz
+    </Button>
+  );
+}
+SubmitButton.propTypes = {
+  state: PropTypes.any.isRequired,
+  onSubmit: EditModalContent.propTypes.onSubmit,
+  onCancel: EditModalContent.propTypes.onCancel,
 };
--- ui/src/components/ExpensesList/EditModalContentEntry.js
+++ ui/src/components/ExpensesList/EditModalContentEntry.js
@@ -7,50 +7,7 @@ import { QUERY_CATEGORIES } from '../CategoriesList/CategoriesList.gql';
 import * as MoneyAmount from '../../model/MoneyAmount';
 import { MoneyAmount as MoneyAmountPropType } from '../../model/propTypes';
 
-export function EditModalContentEntry({ entry, idx, setEntry }) {
-  const { id: budgetID } = useBudget();
-  const { loading, error, data } = useQuery(QUERY_CATEGORIES, {
-    variables: { budgetID },
-  });
-  if (loading) return <p>Loading...</p>;
-  if (error) {
-    console.error(error);
-    return <p>Error :(</p>;
-  }
-  return (
-    <Row>
-      <Col>
-        <Form.Control
-          as="select"
-          value={entry.categoryID || (entry.category && entry.category.id)}
-          onChange={e => setEntry(idx, { categoryID: e.target.value })}
-        >
-          <option></option>
-          {data.categories
-            .sort((c1, c2) => c1.name.localeCompare(c2))
-            .map(category => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-        </Form.Control>
-      </Col>
-      <Col>
-        <Form.Control
-          type="number"
-          placeholder="Kwota"
-          value={MoneyAmount.format(entry.balance)}
-          onChange={e => setEntry(idx, { balance: e.target.value })}
-          onBlur={() =>
-            setEntry(idx, { balance: MoneyAmount.parse(entry.balance) })
-          }
-        />
-      </Col>
-    </Row>
-  );
-}
-
-EditModalContentEntry.propTypes = {
+const propTypes = {
   entry: PropTypes.shape({
     categoryID: PropTypes.any,
     category: PropTypes.shape({
@@ -61,3 +18,64 @@ EditModalContentEntry.propTypes = {
   idx: PropTypes.number.isRequired,
   setEntry: PropTypes.func.isRequired,
 };
+
+export function EditModalContentEntry(props) {
+  return (
+    <Row>
+      <Col>
+        <CategoryInput {...props} />
+      </Col>
+      <Col>
+        <BalanceInput {...props} />
+      </Col>
+    </Row>
+  );
+}
+EditModalContentEntry.propTypes = propTypes;
+
+function CategoryInput({ entry, idx, setEntry }) {
+  const { id: budgetID } = useBudget();
+  const { loading, error, data } = useQuery(QUERY_CATEGORIES, {
+    variables: { budgetID },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.error(error);
+    return <p>Error :(</p>;
+  }
+
+  return (
+    <Form.Control
+      as="select"
+      value={entry.categoryID || (entry.category && entry.category.id)}
+      onChange={e => setEntry(idx, { categoryID: e.target.value })}
+    >
+      <option></option>
+      {data.categories
+        .sort((c1, c2) => c1.name.localeCompare(c2))
+        .map(category => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
+    </Form.Control>
+  );
+}
+
+CategoryInput.propTypes = propTypes;
+
+function BalanceInput({ entry, idx, setEntry }) {
+  return (
+    <Form.Control
+      type="number"
+      placeholder="Kwota"
+      value={MoneyAmount.format(entry.balance)}
+      onChange={e => setEntry(idx, { balance: e.target.value })}
+      onBlur={() =>
+        setEntry(idx, { balance: MoneyAmount.parse(entry.balance) })
+      }
+    />
+  );
+}
+
+BalanceInput.propTypes = propTypes;
