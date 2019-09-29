b9c1296db04f403580afe1cab7fd0af30072081e
Szymon Janota <szymon.janota@sap.com>
2019-09-29 17:59:19 +0200

    Simplify list rendering a bit

--- ui/src/components/common/List/List.js
+++ ui/src/components/common/List/List.js
@@ -3,34 +3,38 @@ import { Table } from './Table';
 import EditModalList from './EditModalList';
 import EditInlineList from './EditInlineList';
 import './List.css';
+import PropTypes from 'prop-types';
 
 List.EditMode = {
   MODAL: 'MODAL',
   INLINE: 'INLINE',
 };
 
-export function List({ editMode, renderHeader, ...props }) {
-  function PartiallyAppliedTable({ children, onCreate }) {
-    return (
-      <Table renderHeader={renderHeader} onCreate={onCreate}>
-        {children}
-      </Table>
-    );
-  }
+function partiallyApplyTable(renderHeader) {
+  // eslint-disable-next-line react/display-name
+  return props => <Table renderHeader={renderHeader} {...props} />;
+}
 
+export function List({ editMode, renderHeader, ...props }) {
+  const table = partiallyApplyTable(renderHeader);
   let el;
   switch (editMode) {
     case List.EditMode.MODAL:
-      el = <EditModalList tableComponent={PartiallyAppliedTable} {...props} />;
+      el = <EditModalList tableComponent={table} {...props} />;
       break;
     case List.EditMode.INLINE:
-      el = <EditInlineList tableComponent={PartiallyAppliedTable} {...props} />;
+      el = <EditInlineList tableComponent={table} {...props} />;
       break;
     default:
   }
 
   return <div className={'List'}>{el}</div>;
 }
+
+List.propTypes = {
+  editMode: PropTypes.oneOf(Object.values(List.EditMode)),
+  renderHeader: PropTypes.func.isRequired,
+};
 
 List.defaultProps = {
   editMode: List.EditMode.INLINE,
